af93770c452dd4d711879a6917a50d84f634c0ba
Bruce <1033684650@qq.com>
2023-07-20 21:18:36 +0800

    add mysql constant variable

---
+++ src/common/constant/mysql.ts
@@ -0,0 +1,6 @@
+export const TYEP = "mysql"
+export const HOST = "localhost"
+export const PORT = 3306
+export const USER_NAME = "root"
+export const PASSWORD = "123456"
+export const DATA_BASE = "instagram"
