315db1e1cd495122c4d7ab113abcf867e14dea16
Bruce <1033684650@qq.com>
2023-07-20 21:24:21 +0800

    config the mysql constant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+// Custom Imports
+import { 
+  TYPE,
+  HOST,
+  PORT,
+  USER_NAME,
+  PASSWORD,
+  DATA_BASE
+} from '@/common/constant/mysql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: TYPE,
+      host: HOST,
+      port: PORT,
+      username: USER_NAME,
+      password: PASSWORD,
+      database: DATA_BASE,
+      entities: [`${__dirname}/../modules/**/*.entity{.ts,tsx}`],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/common/constant/mysql.ts
+++ src/common/constant/mysql.ts
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-07-20 21:19:46
+ * @Author: Bruce
+ * @Description: mysql config
+ */
+
 export const TYEP = "mysql"
 export const HOST = "localhost"
 export const PORT = 3306
