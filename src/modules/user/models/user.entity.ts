78933022ee0f5fc7f00bfc6408c316871f0ba312
Bruce <1033684650@qq.com>
2023-07-20 21:12:59 +0800

    add user.entity & user.service

---
+++ src/modules/user/models/user.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsNotEmpty } from "class-validator";
+
+@Entity("user")
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({ comment: "昵称", default: "" })
+    @IsNotEmpty()
+    name: string;
+
+    @Column({ comment: "描述信息", default: "" })
+    desc: string;
+
+    @Column({ comment: "手机号", nullable: true })
+    tel: string;
+
+    @Column({  comment: "密码", nullable: true})
+    password: string;
+
+    @Column({ comment: "盐", nullable: true })
+    salt: string;
+}
---
+++ src/modules/user/service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm"
+import { Repository, DeepPartial } from "typeorm";
+
+// Custom Imports
+import { User } from "./models/user.entity";
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private UserRepository: Repository<User>
+  ){}
+
+  /**
+   * 新增一个用户
+   * @param entity 用户的实体 
+   * @returns true / false
+   */
+  async create(entity: DeepPartial<User>): Promise<boolean> {
+    const res = await this.UserRepository.insert(entity);
+    if(res && res.raw.affectedRows >0) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 删除一个用户
+   * @param id 用户的id
+   * @returns true / false
+   */
+  async del(id: string): Promise<boolean> {
+    const res = await this.UserRepository.delete(id);
+    if(res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 更新一个用户
+   * @param id 用户的Id
+   * @param entity 用户的实体
+   * @returns 
+   */
+  async update(id: string, entity: DeepPartial<User>): Promise<boolean> {
+    const res = await this.UserRepository.update(id, entity);
+    if(res.affected > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 通过手机号查询用户
+   * @param tel 用户的手机号
+   * @returns 用户的实体
+   */
+  async findByTel(tel: string): Promise<User> {
+    const res = await this.UserRepository.findOne({
+      where: {
+        tel
+      }
+    })
+    return res;
+  }
+}
